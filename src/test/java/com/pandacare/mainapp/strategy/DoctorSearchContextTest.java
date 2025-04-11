7477863f09f7b998462adfed18d3cb2ce44d527f
noQils <daffaaqilmahmud@gmail.com>
2025-04-11 18:04:42 +0700

    [RED] Add tests for SearchStrategy (strategy pattern)

---
+++ src/test/java/com/pandacare/mainapp/strategy/DoctorSearchContextTest.java
@@ -0,0 +1,56 @@
+package com.pandacare.mainapp.strategy;
+
+import com.pandacare.mainapp.model.DoctorProfile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class DoctorSearchContextTest {
+
+    private DoctorSearchContext searchContext;
+    private SearchStrategy mockStrategy;
+    List<DoctorProfile> doctorProfileList;
+
+    @BeforeEach
+    void setUp() {
+        searchContext = new DoctorSearchContext();
+        mockStrategy = mock(SearchStrategy.class);
+        searchContext.setStrategy(mockStrategy);
+        doctorProfileList = new ArrayList<>();
+
+        Map<String, String> workSchedule1 = new HashMap<>();
+        workSchedule1.put("Senin", "09:00-12:00");
+        workSchedule1.put("Rabu", "10:00-13:00");
+
+        DoctorProfile doctorProfile1 = new DoctorProfile("Dr. Hafiz", "hafiz@pandacare.com", "08123456789", "RS Pandacare",
+                workSchedule1, "Cardiologist", 4.9);
+        doctorProfile1.setId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        doctorProfileList.add(doctorProfile1);
+
+        Map<String, String> workSchedule2 = new HashMap<>();
+        workSchedule2.put("Selasa", "14:00-18:00");
+        workSchedule2.put("Kamis", "09:00-12:00");
+
+        DoctorProfile doctorProfile2 = new DoctorProfile("Dr. Jonah", "jonah@pandacare.com", "08192836789", "RS Pondok Indah",
+                workSchedule2, "Orthopedic", 4.8);
+        doctorProfile2.setId("eb558e9f-1c39-460e-8860-71af6af63ds2");
+        doctorProfileList.add(doctorProfile2);
+    }
+
+    @Test
+    void testExecuteSearchUsesCorrectStrategyAndReturnsResults() {
+        DoctorProfile doctorProfile = doctorProfileList.getFirst();
+        List<DoctorProfile> expected = new ArrayList<>();
+        expected.add(doctorProfile);
+        doReturn(expected).when(mockStrategy).search(doctorProfile.getName());
+
+        List<DoctorProfile> result = searchContext.executeSearch(doctorProfile.getName());
+
+        assertEquals(expected, result);
+        verify(mockStrategy, times(1)).search(doctorProfile.getName());
+    }
+}
---
+++ src/test/java/com/pandacare/mainapp/strategy/SearchBySpecialityTest.java
@@ -0,0 +1,62 @@
+package com.pandacare.mainapp.strategy;
+
+import com.pandacare.mainapp.model.DoctorProfile;
+import com.pandacare.mainapp.repository.DoctorProfileRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchBySpecialityTest {
+
+    SearchBySpeciality searchBySpeciality;
+    DoctorProfileRepository doctorProfileRepository;
+    List<DoctorProfile> doctorProfileList;
+
+    @BeforeEach
+    void setUp() {
+        doctorProfileRepository = mock(DoctorProfileRepository.class);
+        searchBySpeciality = new SearchBySpeciality(doctorProfileRepository);
+        doctorProfileList = new ArrayList<>();
+
+        Map<String, String> workSchedule1 = new HashMap<>();
+        workSchedule1.put("Senin", "09:00-12:00");
+        workSchedule1.put("Rabu", "10:00-13:00");
+
+        DoctorProfile doctorProfile1 = new DoctorProfile("Dr. Hafiz", "hafiz@pandacare.com", "08123456789", "RS Pandacare",
+                workSchedule1, "Cardiologist", 4.9);
+        doctorProfile1.setId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        doctorProfileList.add(doctorProfile1);
+
+        Map<String, String> workSchedule2 = new HashMap<>();
+        workSchedule2.put("Selasa", "14:00-18:00");
+        workSchedule2.put("Kamis", "09:00-12:00");
+
+        DoctorProfile doctorProfile2 = new DoctorProfile("Dr. Jonah", "jonah@pandacare.com", "08192836789", "RS Pondok Indah",
+                workSchedule2, "Orthopedic", 4.8);
+        doctorProfile2.setId("eb558e9f-1c39-460e-8860-71af6af63ds2");
+        doctorProfileList.add(doctorProfile2);
+    }
+
+    @Test
+    void testSearchBySpeciality() {
+        DoctorProfile doctorProfile = doctorProfileList.get(1);
+        List<DoctorProfile> expected = new ArrayList<>();
+        expected.add(doctorProfile);
+        doReturn(expected).when(doctorProfileRepository).findBySpeciality(doctorProfile.getSpeciality());
+
+        List<DoctorProfile> result = searchBySpeciality.search(doctorProfile.getSpeciality());
+
+        assertEquals(expected, result);
+        verify(doctorProfileRepository, times(1)).findBySpeciality(doctorProfile.getSpeciality());
+    }
+}
