2b48c1ad1df4ae45ac1be343feb86a09e03514c7
FaiziIsmady <faiziismady5@gmail.com>
2025-05-02 20:06:29 +0700

    [RED] Add necessary imports for JadwalPacilianControllerTest

--- src/test/java/com/pandacare/mainapp/jadwal/controller/JadwalPacilianControllerTest.java
+++ src/test/java/com/pandacare/mainapp/jadwal/controller/JadwalPacilianControllerTest.java
@@ -1,11 +1,19 @@
 package com.pandacare.mainapp.jadwal.controller;
 
+import com.pandacare.mainapp.jadwal.model.JadwalKonsultasi;
 import com.pandacare.mainapp.jadwal.service.JadwalPacilianServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(JadwalPacilianController.class)
 class JadwalPacilianControllerTest {
