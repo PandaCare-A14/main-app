408b3a61223f85a592b2290b6716960ce5da3342
noQils <daffaaqilmahmud@gmail.com>
2025-05-02 14:54:22 +0700

    [GREEN] Add dependency injection in search strategies to implement the updated searchDoctorProfile function in service

--- src/main/java/com/pandacare/mainapp/doctor_profile/strategy/SearchByName.java
+++ src/main/java/com/pandacare/mainapp/doctor_profile/strategy/SearchByName.java
@@ -2,9 +2,11 @@ package com.pandacare.mainapp.doctor_profile.strategy;
 
 import com.pandacare.mainapp.doctor_profile.model.DoctorProfile;
 import com.pandacare.mainapp.doctor_profile.repository.DoctorProfileRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component("nameSearchStrategy")
 public class SearchByName implements SearchStrategy {
     private DoctorProfileRepository doctorProfileRepository;
 
