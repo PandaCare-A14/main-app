2b918ca192310e44e6aaf4e2ed92b05b68752e57
freiazulaika <freiazulaika27@gmail.com>
2025-05-06 15:08:02 +0700

    [GREEN] Make complete implementation

--- src/main/java/com/pandacare/mainapp/konsultasi_dokter/model/JadwalKonsultasi.java
+++ src/main/java/com/pandacare/mainapp/konsultasi_dokter/model/JadwalKonsultasi.java
@@ -4,12 +4,13 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.Getter;
 import lombok.Setter;
+import com.pandacare.mainapp.konsultasi_dokter.model.state.StatusJadwalDokter;
+import com.pandacare.mainapp.konsultasi_dokter.model.state.AvailableState;
 
 @Setter
 @Getter
 public class JadwalKonsultasi {
     private String id;
-    private String statusDokter;
     private String statusPacilian;
     private String idDokter;
     private String idPasien;
@@ -19,12 +20,37 @@ public class JadwalKonsultasi {
     private String note;
     private String message;
     private boolean changeSchedule = false;
+    private StatusJadwalDokter currentState;
 
     public JadwalKonsultasi() {
-        this.statusDokter = "AVAILABLE";
+        this.currentState = new AvailableState();
+    }
+
+    public void request(String idPasien, String message) {
+        currentState.handleRequest(this, idPasien, message);
+    }
+
+    public void approve() {
+        currentState.handleApprove(this);
+    }
+
+    public void reject(String reason) {
+        currentState.handleReject(this, reason);
+    }
+
+    public void changeSchedule(LocalDate day, LocalTime start, LocalTime end, String reason) {
+        currentState.handleChangeSchedule(this, day, start, end, reason);
+    }
+
+    public void setState(StatusJadwalDokter state) {
+        this.currentState = state;
+    }
+
+    public String getStatusDokter() {
+        return currentState.getStatusName();
     }
 
     public boolean isAvailable() {
-        return "AVAILABLE".equals(statusDokter);
+        return currentState.isAvailable();
     }
 }
