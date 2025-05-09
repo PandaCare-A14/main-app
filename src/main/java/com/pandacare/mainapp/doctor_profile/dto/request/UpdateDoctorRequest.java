0bb20fca4ce521b575742c60e72901f979fddeba
noQils <daffaaqilmahmud@gmail.com>
2025-05-09 20:16:50 +0700

    [GREEN] Implement update DoctorProfile request dto

---
+++ src/main/java/com/pandacare/mainapp/doctor_profile/dto/request/UpdateDoctorRequest.java
@@ -0,0 +1,29 @@
+package com.pandacare.mainapp.doctor_profile.dto.request;
+
+import lombok.Data;
+import jakarta.validation.constraints.*;
+import java.util.Map;
+
+@Data
+public class UpdateDoctorRequest {
+    @NotBlank(message = "ID is required")
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9\\s-]+$", message = "Invalid phone number format")
+    private String phoneNumber;
+
+    private String workAddress;
+
+    @NotNull(message = "Work schedule is required")
+    private Map<String, String> workSchedule;
+
+    @NotBlank(message = "Speciality is required")
+    private String speciality;
+}
