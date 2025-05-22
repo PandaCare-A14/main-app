e599cdd30b6793802090c81b2fdd8f08f92fc8fb
noQils <daffaaqilmahmud@gmail.com>
2025-05-22 20:55:16 +0700

    Modified application properties

--- src/test/java/com/pandacare/mainapp/MainappApplicationTests.java
+++ src/test/java/com/pandacare/mainapp/MainappApplicationTests.java
@@ -3,11 +3,11 @@ package com.pandacare.mainapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class MainappApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class MainappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
