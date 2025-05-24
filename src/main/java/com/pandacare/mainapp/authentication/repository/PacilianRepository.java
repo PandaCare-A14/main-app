10ee8bdb7cc6f29229aa89c5dcaf032b823e2006
noQils <daffaaqilmahmud@gmail.com>
2025-05-24 20:59:38 +0700

    Deleted unused files

--- src/main/java/com/pandacare/mainapp/authentication/repository/PacilianRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pandacare.mainapp.authentication.repository;
-
-import com.pandacare.mainapp.authentication.model.Pacilian;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PacilianRepository extends JpaRepository<Pacilian, UUID> {}
